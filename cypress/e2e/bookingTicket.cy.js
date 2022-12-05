4f860cfe4cbeb8a8e8ed93ed8561587e330cc6d2
Sergiu Contevici <sergiucontevici@gmail.com>
2022-12-05 21:52:12 +0200

    Fixes

--- cypress/e2e/bookingTicket.cy.js
+++ cypress/e2e/bookingTicket.cy.js
@@ -1,7 +1,11 @@
 /// <reference types="Cypress" />
 
-const seats = require('../fixtures/seats.json');
-const { daysOfWeek, movieSeance, acceptionButton } = require('../fixtures/cinemaHallSelectors.json')
+const seats = require("../fixtures/seats.json");
+const {
+  daysOfWeek,
+  movieSeance,
+  acceptionButton,
+} = require("../fixtures/cinemaHallSelectors.json");
 
 describe("The user can booking the ticket", () => {
   beforeEach(() => {
@@ -11,16 +15,13 @@ describe("The user can booking the ticket", () => {
   it("User have possible to booking the ticket", () => {
     cy.get(daysOfWeek).eq(2).click();
     cy.get(movieSeance).click();
-    cy.get(acceptionButton).should('be.disabled');
+    cy.get(acceptionButton).should("be.disabled");
+    cy.selectFreeSlots();
 
-    seats.forEach((element) => {
-      cy.get(`.buying-scheme__wrapper > :nth-child(${element.row}) > :nth-child(${element.seat})`).click();
-    })
-
-    cy.get(acceptionButton).should('not.be.disabled').click();
-    cy.get('.ticket__check-title').should('be.visible');
+    cy.get(acceptionButton).should("not.be.disabled").click();
+    cy.get(".ticket__check-title").should("be.visible");
     cy.checkTicketInformation();
     cy.get(acceptionButton).click();
-    cy.get('.ticket__info-qr').should('be.visible');
+    cy.get(".ticket__info-qr").should("be.visible");
   });
-})
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,35 +25,93 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 // const { movieTitle, movieSynopsis, movieTimeDuration, movieDataOrigin} = require('../fixtures/selectors');
-const { firstMovieTitle, 
-    firstMovieSynopsis, 
-    firstMovieTimeDuration, 
-    firstMovieDataOrigin, 
-    secondMovieTitle, 
-    secondMovieSynopsis, 
-    secondMovieTimeDuration, 
-    secondMovieDataOrigin 
-} = require('../fixtures/movies.json');
+const {
+  firstMovieTitle,
+  firstMovieSynopsis,
+  firstMovieTimeDuration,
+  firstMovieDataOrigin,
+  secondMovieTitle,
+  secondMovieSynopsis,
+  secondMovieTimeDuration,
+  secondMovieDataOrigin,
+} = require("../fixtures/movies.json");
 
-Cypress.Commands.add('checkMovieInformation', (type) => {
-    switch(type) {
-        case "logan":
-            cy.get(".movie").find('div > div > h2').eq(0).should('have.text', firstMovieTitle);
-            cy.get(".movie").find('div > div').eq(1).find('p').eq(0).should('have.text', firstMovieSynopsis);
-            cy.get(".movie").find('div > div').eq(1).find('p > span').eq(0).should('have.text', firstMovieTimeDuration);
-            cy.get(".movie").find('div > div').eq(1).find('p > span').eq(1).should('have.text', firstMovieDataOrigin);
-        case "Movie 3":
-            cy.get(".movie").find('div > div').eq(3).find('h2').should('have.text', secondMovieTitle);
-            cy.get(".movie").find('div > div').eq(3).find('p').eq(0).should('have.text', secondMovieSynopsis);
-            cy.get(".movie").find('div > div').eq(3).find('p > span').eq(0).should('have.text', secondMovieTimeDuration);
-            cy.get(".movie").find('div > div').eq(3).find('p > span').eq(1).should('have.text', secondMovieDataOrigin);
-    }
-})
+const { freeSlots } = require("../fixtures/cinemaHallSelectors.json");
 
-Cypress.Commands.add('checkTicketInformation', () => {
-    cy.get(".ticket__info-wrapper").find('p').eq(0).should('have.text', 'На фильм: Логан');
-    cy.get(".ticket__info-wrapper").find('p').eq(1).should('have.text', 'Ряд/Место: 2/3, 3/4, 4/4, 5/6, 7/8')
-    cy.get(".ticket__info-wrapper").find('p').eq(2).should('have.text', 'В зале: Зал 1')
-    cy.get(".ticket__info-wrapper").find('p').eq(4).should('have.text', 'Начало сеанса: 19:00')
-    cy.get(".ticket__info-wrapper").find('p').eq(5).should('have.text', 'Стоимость: 750 руб.')
-})
+Cypress.Commands.add("checkMovieInformation", (type) => {
+  switch (type) {
+    case "logan":
+      cy.get(".movie")
+        .find("div > div > h2")
+        .eq(0)
+        .should("have.text", firstMovieTitle);
+      cy.get(".movie")
+        .find("div > div")
+        .eq(1)
+        .find("p")
+        .eq(0)
+        .should("have.text", firstMovieSynopsis);
+      cy.get(".movie")
+        .find("div > div")
+        .eq(1)
+        .find("p > span")
+        .eq(0)
+        .should("have.text", firstMovieTimeDuration);
+      cy.get(".movie")
+        .find("div > div")
+        .eq(1)
+        .find("p > span")
+        .eq(1)
+        .should("have.text", firstMovieDataOrigin);
+    case "Movie 3":
+      cy.get(".movie")
+        .find("div > div")
+        .eq(3)
+        .find("h2")
+        .should("have.text", secondMovieTitle);
+      cy.get(".movie")
+        .find("div > div")
+        .eq(3)
+        .find("p")
+        .eq(0)
+        .should("have.text", secondMovieSynopsis);
+      cy.get(".movie")
+        .find("div > div")
+        .eq(3)
+        .find("p > span")
+        .eq(0)
+        .should("have.text", secondMovieTimeDuration);
+      cy.get(".movie")
+        .find("div > div")
+        .eq(3)
+        .find("p > span")
+        .eq(1)
+        .should("have.text", secondMovieDataOrigin);
+  }
+});
+
+Cypress.Commands.add("checkTicketInformation", () => {
+  cy.get(".ticket__info-wrapper")
+    .find("p")
+    .eq(0)
+    .should("have.text", "На фильм: Логан");
+  cy.get(".ticket__info-wrapper")
+    .find("p")
+    .eq(2)
+    .should("have.text", "В зале: Зал 1");
+  cy.get(".ticket__info-wrapper")
+    .find("p")
+    .eq(4)
+    .should("have.text", "Начало сеанса: 19:00");
+  cy.get(".ticket__info-wrapper")
+    .find("p")
+    .eq(5)
+    .should("have.text", "Стоимость: 750 руб.");
+});
+
+Cypress.Commands.add("selectFreeSlots", () => {
+  for (let i = 1; i <= 5; i++) {
+    let index = i - 1;
+    cy.get(freeSlots).eq(i).click();
+  }
+});
